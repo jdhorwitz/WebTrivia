a7bba10123c290971f0600a9a7b36f25aebb5694
Josh Horwitz <joshua.d.horwitz@gmail.com>
2015-07-05 23:25:59 -0400

    Added in more classes

--- src/main/java/com/trivia/spring/service/PlayerService.java
+++ src/main/java/com/trivia/spring/service/PlayerService.java
@@ -1,5 +1,18 @@
 package com.trivia.spring.service;
-
-public class PlayerService {
-
+ 
+import java.util.List;
+ 
+import com.trivia.spring.model.Player;
+ 
+public interface PlayerService {
+ 
+    void savePlayer(Player player);
+ 
+    List<Player> findAllPlayers();
+ 
+    void deletePlayerById(String id);
+ 
+    Player findById(String id);
+ 
+    void updatePlayer(Player player);
 }
