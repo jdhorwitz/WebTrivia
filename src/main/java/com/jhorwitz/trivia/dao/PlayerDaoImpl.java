510f2b3996fb407814e6b1ea3a225c7745704bc2
Josh Horwitz <joshua.d.horwitz@gmail.com>
2015-07-10 22:48:28 -0400

    Edited DAO

--- src/main/java/com/jhorwitz/trivia/dao/PlayerDaoImpl.java
+++ src/main/java/com/jhorwitz/trivia/dao/PlayerDaoImpl.java
@@ -2,33 +2,30 @@ package com.jhorwitz.trivia.dao;
 
 
 import com.jhorwitz.trivia.model.Player;
-import org.hibernate.Criteria;
 import org.springframework.stereotype.Repository;
-
 import javax.persistence.*;
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository("PlayerDao")
+@Transactional
 public class PlayerDaoImpl extends AbstractDao implements PlayerDao {
 
-    EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("trivia");
-    EntityManager em = entityManagerFactory.createEntityManager();
-    EntityTransaction userTransaction = em.getTransaction();
+    @PersistenceContext
+    private EntityManager em;
 
-    @Override
-    public void savePlayer(Player player) {
-
+    public Long savePlayer(Player player){
+        em.persist(player);
+        return player.getId();
     }
+
 
     @Override
     public List<Player> findAllPlayers() {
-        TypedQuery<Player> query = em.createQuery("", Player.class);
+        TypedQuery<Player> query = em.createQuery("SELECT p FROM Player p", Player.class);
         List<Player> list = query.getResultList();
-        Player p = new Player();
-        for (int i = 0; i < list.size(); i++) {
-            p = list.get(i);
-        }
-        return (List<Player>) p;
+        return list;
+
     }
 
     @Override
@@ -37,16 +34,6 @@ public class PlayerDaoImpl extends AbstractDao implements PlayerDao {
     }
 
 
-    public void createPlayer(long id, String name, int score, String team, String username) {
-        userTransaction.begin();
-        Player player = new Player();
-        player.setId(id);
-        player.setName(name);
-        player.setScore(score);
-        player.setTeam(team);
-        player.setUsername(username);
-        em.persist(player);
-        userTransaction.commit();
     }
 
     public Player getPlayer(String id) {
