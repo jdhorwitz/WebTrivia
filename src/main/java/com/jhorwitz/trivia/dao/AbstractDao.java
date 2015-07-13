2fa9c2fc1c7c702d1e72eff57d3d54ab578c7569
Josh Horwitz <joshua.d.horwitz@gmail.com>
2015-07-12 22:25:16 -0400

    fixed abstract dao

--- src/main/java/com/jhorwitz/trivia/dao/AbstractDao.java
+++ src/main/java/com/jhorwitz/trivia/dao/AbstractDao.java
@@ -3,21 +3,29 @@ package com.jhorwitz.trivia.dao;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
- 
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public abstract class AbstractDao {
  
     @Autowired
     private SessionFactory sessionFactory;
+
+    @Autowired
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("PlayerServiceImpl");
+
  
-    protected Session getSession() {
-        return sessionFactory.getCurrentSession();
+    protected EntityManager getEntityManager() {
+        return emf.createEntityManager();
     }
  
     public void persist(Object entity) {
-        getSession().persist(entity);
+        getEntityManager().persist(entity);
     }
  
     public void delete(Object entity) {
-        getSession().delete(entity);
+        getEntityManager().remove(entity);
     }
 }
