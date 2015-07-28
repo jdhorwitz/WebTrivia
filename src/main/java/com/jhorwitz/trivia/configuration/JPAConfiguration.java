4f3ff8f145074c0f94b378b736b0a0a55a687241
Joshua Horwitz <joshua.d.horwitz@gmail.com>
2015-07-28 12:37:14 -0400

    Updated JPA Config and Maven

--- src/main/java/com/jhorwitz/trivia/configuration/JPAConfiguration.java
+++ src/main/java/com/jhorwitz/trivia/configuration/JPAConfiguration.java
@@ -1,5 +1,7 @@
 package com.jhorwitz.trivia.configuration;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -7,22 +9,52 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @ComponentScan({ "com.jhorwitz.trivia.configuration" })
 @PropertySource(value = { "classpath:application.properties" })
 public class JPAConfiguration {
 
-    static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("WebTrivia");
-    static EntityManager em = entityManagerFactory.createEntityManager();
-    static EntityTransaction userTransaction = em.getTransaction();
+    @Bean(destroyMethod = "close")
+    DataSource dataSource(Environment env){
+        HikariConfig dataSourceConfig = new HikariConfig();
+        dataSourceConfig.setDriverClassName(env.getRequiredProperty("db.driver"));
+        dataSourceConfig.setJdbcUrl(env.getRequiredProperty("db.url"));
+        dataSourceConfig.setUsername(env.getRequiredProperty("db.username"));
+        dataSourceConfig.setPassword(env.getRequiredProperty("db.password"));
+
+        return new HikariDataSource(dataSourceConfig);
+    }
+
+    LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource, Environment env) {
+
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setDataSource(dataSource);
+        entityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        entityManagerFactoryBean.setPackagesToScan("com.jhorwitz.trivia");
+
+        Properties jpaProperties = new Properties();
+
+        jpaProperties.put("hibernate.dialect", env.getRequiredProperty("hibernate.dialect"));
+        jpaProperties.put("hibernate.hbm2ddl.auto", env.getRequiredProperty("hibernate.hbm2ddl.auto"));
+        jpaProperties.put("hibernate.ejb.naming_strategy", env.getRequiredProperty("hibernate.ejb.naming_strategy"));
+        jpaProperties.put("hibernate.show_sql", env.getRequiredProperty("hibernate.show_sql"));
+        jpaProperties.put("hibernate.format_sql", env.getRequiredProperty("hibernate.format_sql"));
+
+        entityManagerFactoryBean.setJpaProperties(jpaProperties);
+
+        return entityManagerFactoryBean;
+    }
 
 
 }
